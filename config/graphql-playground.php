b2d6c05fe1132f50e9579c1482f244ccbd9dba55
Eduardo Lira <eduardolirainfo@gmail.com>
2020-06-14 16:37:08 -0300

    gp update

--- config/graphql-playground.php
+++ config/graphql-playground.php
@@ -1,43 +1,18 @@
 <?php
 
-declare(strict_types=1);
-
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Route configuration
-    |--------------------------------------------------------------------------
-    |
-    | Set the URI at which the GraphQL Playground can be viewed
-    | and any additional configuration for the route.
-    |
-    */
+    // Route for the frontend
+    'route' => '/',
 
-    'route' => 'graphql-playground',
+    // Which middleware to apply, if any
+    'middleware' => [
+        // 'web',
+    ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default GraphQL endpoint
-    |--------------------------------------------------------------------------
-    |
-    | The default endpoint that the Playground UI is set to.
-    | It assumes you are running GraphQL on the same domain
-    | as GraphQL Playground, but can be set to any URL.
-    |
-    */
+    // Route for the GraphQL endpoint
+    'endpoint' => 'graphql',
 
-    'endpoint' => '/graphql',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Control Playground availability
-    |--------------------------------------------------------------------------
-    |
-    | Control if the playground is accessible at all.
-    | This allows you to disable it in certain environments,
-    | for example you might not want it active in production.
-    |
-    */
-
+    // Control if the playground is accessible at all
+    // This allows you to disable it completely in production
     'enabled' => env('GRAPHQL_PLAYGROUND_ENABLED', true),
 ];
